2532556fd0bd4541a452fb6aa5d0a7ee9d575c17
Vladimir <vladimirsouchard@yahoo.com.br>
2016-11-14 00:03:05 -0200

    Versão 1.1

--- app/src/main/java/com/souchard/alcoolxgasolina/CalculoActivity.java
+++ app/src/main/java/com/souchard/alcoolxgasolina/CalculoActivity.java
@@ -1,5 +1,6 @@
 package com.souchard.alcoolxgasolina;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -41,22 +42,47 @@ public class CalculoActivity extends AppCompatActivity implements View.OnClickLi
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.buttonCalc:
-             double VKilometragem = Double.parseDouble(editTextKilometragem.getText().toString());
-             double QLitros = Double.parseDouble(editTextLitros.getText().toString());
-             double calculo = VKilometragem / QLitros;
-                //Ocultando o teclado
-                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(editTextKilometragem.getWindowToken(), 0);
-               // Double resultado = calculo;
-                BigDecimal calculo_extato = new BigDecimal(calculo).setScale(2, RoundingMode.HALF_DOWN);
-                BigDecimal Kilometragem = new BigDecimal(VKilometragem).setScale(0, RoundingMode.HALF_DOWN);
-                BigDecimal Litros = new BigDecimal(QLitros).setScale(0, RoundingMode.HALF_DOWN);
-                textViewResultado.setText("Com " + Litros + " litros de combustível \n seu carro fez " + Kilometragem + "km eo \n consumo médio foi de " + calculo_extato +  " km/l.");
-                editTextKilometragem.setText("");
-                editTextLitros.setText("");
-                editTextKilometragem.requestFocus();
+                if (editTextKilometragem.getText().toString().equals("") && editTextLitros.getText().toString().equals("")) {
+                    AlertDialog.Builder dialogo = new
+                            AlertDialog.Builder(CalculoActivity.this);
+                    dialogo.setTitle("ALERTA");
+                    dialogo.setMessage("Informe a distância percorrida e os litros gastos.");
+                    dialogo.setNeutralButton("OK", null);
+                    dialogo.show();
+                    editTextKilometragem.requestFocus();
+                } else if (editTextKilometragem.getText().toString().equals("")){
+                    AlertDialog.Builder dialogo = new
+                            AlertDialog.Builder(CalculoActivity.this);
+                    dialogo.setTitle("ALERTA");
+                    dialogo.setMessage("Informe a distância percorrida.");
+                    dialogo.setNeutralButton("OK", null);
+                    dialogo.show();
+                    editTextKilometragem.requestFocus();
+                } else if (editTextLitros.getText().toString().equals("")) {
+                    AlertDialog.Builder dialogo = new
+                            AlertDialog.Builder(CalculoActivity.this);
+                    dialogo.setTitle("ALERTA");
+                    dialogo.setMessage("Informe a quantidade de litros de combustível gasto.");
+                    dialogo.setNeutralButton("OK", null);
+                    dialogo.show();
+                    editTextLitros.requestFocus();
+                }else {
+                    double VKilometragem = Double.parseDouble(editTextKilometragem.getText().toString());
+                    double QLitros = Double.parseDouble(editTextLitros.getText().toString());
+                    double calculo = VKilometragem / QLitros;
+                    //Ocultando o teclado
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(editTextKilometragem.getWindowToken(), 0);
+                    // Double resultado = calculo;
+                    BigDecimal calculo_extato = new BigDecimal(calculo).setScale(2, RoundingMode.HALF_DOWN);
+                    BigDecimal Kilometragem = new BigDecimal(VKilometragem).setScale(0, RoundingMode.HALF_DOWN);
+                    BigDecimal Litros = new BigDecimal(QLitros).setScale(0, RoundingMode.HALF_DOWN);
+                    textViewResultado.setText("Com " + Litros + " litros de combustível \n seu carro fez " + Kilometragem + "km eo \n consumo médio foi de " + calculo_extato + " km/l.");
+                    editTextKilometragem.setText("");
+                    editTextLitros.setText("");
+                    editTextKilometragem.requestFocus();
+                }
                 break;
-
             case R.id.buttonLimpar:
                 editTextKilometragem.setText("");
                 editTextLitros.setText("");
